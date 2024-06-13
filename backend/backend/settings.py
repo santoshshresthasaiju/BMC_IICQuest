4ac86f5bacc5383af7827a090865c61df9c3ffb3
Santosh Shrestha <santoshshrestha002@gmail.com>
2024-06-13 23:57:14 +0545

    final commited successfully

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,11 +90,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Mmse',
+        'USER': 'SMS',
+        'PASSWORD': 'Nepal',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
