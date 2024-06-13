664188b1962b34935ca20e47ce51dd185dbbb905
Santosh Shrestha <santoshshrestha002@gmail.com>
2024-06-13 23:05:49 +0545

    update code of ss

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -17,12 +17,12 @@ return (
 
                 </Link>
                 <div className="flex items-center lg:order-2">
-                    <Link
+                    {/* <Link
                         to="/"
                         className="text-white bg-teal-400 hover:bg-teal-500 focus:ring-4 focus:ring-teal-400 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                     >
                         Online Chat
-                    </Link>
+                    </Link> */}
                 </div>
                 <div
                     className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
