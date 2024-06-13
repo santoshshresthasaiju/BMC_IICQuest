2dc814785b0985d1f532bde75c68a82f25fcd868
Manisha Magar <magarmanisha248@gmail.com>
2024-06-13 17:18:56 +0545

    MM-TASK-4: updated css and solving error

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import Logo from '../../assets/logo0.png'
+import Logo from '../../assets/logo1.png'
 
 export default function Footer() {
     return (
